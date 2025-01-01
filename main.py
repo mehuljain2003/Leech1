e98bffc459338370539638c2a725f99bdf802349
mehuljain2003 <85852792+mehuljain2003@users.noreply.github.com>
2025-01-01 09:40:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ bot = Client(
 
 API_ID    = os.environ.get("API_ID", "22940312")
 API_HASH  = os.environ.get("API_HASH", "82fb5cd2de25e4c01021b8bbae9909bb")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8062254368:AAE0Tf5lwvtpPbGb-r5hEFi3t4Kso5Tc3mE") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7996739561:AAGhbJD_OuOEMs2KZYI7iOENUUYDKYHq6cM") 
   
 import random
 
